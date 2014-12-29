eaf3ef32324bd1a6e2e17669042098adbf61153e
Mathew Kurian <bluejamesbond@gmail.com>
2014-12-29 04:02:11 -0600

    Fixed button states

--- wear/src/main/java/com/fiftyeightmorris/nailbiter/AlertActivity.java
+++ wear/src/main/java/com/fiftyeightmorris/nailbiter/AlertActivity.java
@@ -20,7 +20,7 @@ public class AlertActivity extends Activity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
                 WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
                 WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-        setContentView(R.layout.activity_display);
+        setContentView(R.layout.display_activity);
         mTextView = (TextView) findViewById(R.id.text);
     }
 }
--- wear/src/main/java/com/fiftyeightmorris/nailbiter/UIManager.java
+++ wear/src/main/java/com/fiftyeightmorris/nailbiter/UIManager.java
@@ -33,7 +33,11 @@ public class UIManager {
         mPreInflation = new ArrayList<>();
     }
 
-    public void init(WatchViewStub stub) {
+    public void init(final WatchViewStub stub) {
+
+        WindowManager windowManager = (WindowManager) stub.getContext().getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(mMetrics);
+
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
@@ -48,18 +52,57 @@ public class UIManager {
             }
         });
 
-        WindowManager windowManager = (WindowManager) stub.getContext().getSystemService(Context.WINDOW_SERVICE);
-        windowManager.getDefaultDisplay().getMetrics(mMetrics);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                stub.findViewById(R.id.lasttime_button).setSelected(true);
+            }
+        });
     }
 
-    public int rotateHue(int color, float shift) {
+    @SuppressWarnings("unused")
+    public static int shiftHue(int color, float shift) {
 
         float[] hsv = new float[3];
 
         Color.RGBToHSV((color >> 16) & 0xFF, (color >> 8) & 0xFF, color & 0xFF, hsv);
-        hsv[0] = (hsv[0] + shift) % 360.0f;
+        hsv[0] = (hsv[0] + shift) % 360f;
 
         return Color.HSVToColor((color >> 24) & 0xFF, hsv);
+    }
+
+    @SuppressWarnings("unused")
+    public static int contrast(int color, double contrast) {
+
+        contrast = Math.pow((100 + contrast) / 100, 2);
+
+        int A = (color >> 24) & 0xFF;
+        int R = (color >> 16) & 0xFF;
+        int G = (color >> 8) & 0xFF;
+        int B = color & 0xFF;
+
+        R = (int) (((((R / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+        if (R < 0) {
+            R = 0;
+        } else if (R > 255) {
+            R = 255;
+        }
+
+        G = (int) (((((G / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+        if (G < 0) {
+            G = 0;
+        } else if (G > 255) {
+            G = 255;
+        }
+
+        B = (int) (((((B / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+        if (B < 0) {
+            B = 0;
+        } else if (B > 255) {
+            B = 255;
+        }
+
+        return Color.argb(A, R, G, B);
     }
 
     public final void runOnUiThread(Runnable action) {
@@ -82,10 +125,10 @@ public class UIManager {
 
     public void setHue(final WatchViewStub stub, final float hue) {
 
-        final int themeColor = rotateHue(stub.getResources().getColor(R.color.universal__appcolor), hue);
-        final int bgStartColor = rotateHue(stub.getResources().getColor(R.color.app__background_startcolor), hue);
-        final int bgCenterColor = rotateHue(stub.getResources().getColor(R.color.app__background_centercolor), hue);
-        final int bgEndColor = rotateHue(stub.getResources().getColor(R.color.app__background_endcolor), hue);
+        final int appColor = shiftHue(stub.getResources().getColor(R.color.universal__appcolor), hue);
+        final int bgStartColor = shiftHue(stub.getResources().getColor(R.color.app__background_startcolor), hue);
+        final int bgCenterColor = shiftHue(stub.getResources().getColor(R.color.app__background_centercolor), hue);
+        final int bgEndColor = shiftHue(stub.getResources().getColor(R.color.app__background_endcolor), hue);
 
         final Drawable bgDrawable;
 
@@ -115,29 +158,29 @@ public class UIManager {
                 // ---
                 stateListDrawable = new StateListDrawable();
 
-                layerDrawable = (LayerDrawable) stub.getResources().getDrawable(R.drawable.big_button__background_default);
-                gradientDrawable = (GradientDrawable) layerDrawable.findDrawableByLayerId(R.id.big_button__background_default_backgrounditem);
-                gradientDrawable.setColor(themeColor);
-                stateListDrawable.addState(new int[]{}, layerDrawable);
-
                 layerDrawable = (LayerDrawable) stub.getResources().getDrawable(R.drawable.big_button__background_pressed);
                 gradientDrawable = (GradientDrawable) layerDrawable.findDrawableByLayerId(R.id.big_button__background_pressed_backgrounditem);
-                gradientDrawable.setColor(themeColor);
+                gradientDrawable.setColor(appColor);
                 stateListDrawable.addState(new int[]{android.R.attr.state_pressed}, layerDrawable);
+
+                layerDrawable = (LayerDrawable) stub.getResources().getDrawable(R.drawable.big_button__background_default);
+                gradientDrawable = (GradientDrawable) layerDrawable.findDrawableByLayerId(R.id.big_button__background_default_backgrounditem);
+                gradientDrawable.setColor(appColor);
+                stateListDrawable.addState(new int[]{}, layerDrawable);
 
                 stub.findViewById(R.id.menu_button).setBackground(stateListDrawable);
 
                 // ---
                 layerDrawable = (LayerDrawable) stub.findViewById(R.id.goal_container).getBackground();
                 gradientDrawable = (GradientDrawable) layerDrawable.findDrawableByLayerId(R.id.big_button__background_default_backgrounditem);
-                gradientDrawable.setColor(themeColor);
+                gradientDrawable.setColor(appColor);
 
                 // ---
                 stub.findViewById(R.id.bg).setBackground(bgDrawable);
 
                 // ---
-                ((TextView) stub.findViewById(R.id.today_label)).setTextColor(themeColor);
-                ((TextView) stub.findViewById(R.id.detail_value)).setTextColor(themeColor);
+                ((TextView) stub.findViewById(R.id.today_label)).setTextColor(appColor);
+                ((TextView) stub.findViewById(R.id.detail_value)).setTextColor(appColor);
             }
         });
     }
